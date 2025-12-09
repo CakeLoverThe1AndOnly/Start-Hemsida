de7925b17f46256b42b917e85436e95f1a04129e
CakeLoverThe1AndOnly <emil.nilsson3@elev.montessorimondial.se>
2025-12-09 12:53:44 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const cards = document.querySelectorAll('.card');
+let current = 0;
+
+function showNextCard() {
+  cards[current].classList.remove('active');
+  current = (current + 1) % cards.length;
+  cards[current].classList.add('active');
+}
+
+// Byt kort var 4:e sekund
+setInterval(showNextCard, 4000);
